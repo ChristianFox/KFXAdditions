e17ade63e76598b4b849b544158d3e72801a3db8
Eyeye <christianfox890@icloud.com>
2016-05-05 10:49:29 +0100

    Add all files

---
+++ Example/Pods/Target Support Files/KFXAdditions/KFXAdditions-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double KFXAdditionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char KFXAdditionsVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-KFXAdditions_Example/Pods-KFXAdditions_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_KFXAdditions_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KFXAdditions_ExampleVersionString[];
+
