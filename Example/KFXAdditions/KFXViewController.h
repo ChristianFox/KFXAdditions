0330c397a8604cbd86454832a93c0c51dd1eb11c
Eyeye <christianfox890@icloud.com>
2016-05-05 10:48:37 +0100

    Initial commit

---
+++ Example/KFXAdditions/KFXViewController.h
@@ -0,0 +1,13 @@
+//
+//  KFXViewController.h
+//  KFXAdditions
+//
+//  Created by Christian Fox on 05/05/2016.
+//  Copyright (c) 2016 Christian Fox. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KFXViewController : UIViewController
+
+@end
