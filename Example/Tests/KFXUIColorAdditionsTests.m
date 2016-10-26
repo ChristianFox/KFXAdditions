f7bcd87ef4cf64bab89073483cbe999ad7678d3a
Christian Fox <christianfox890@icloud.com>
2016-10-26 15:26:38 +0100

    Adds UIColor+KFXAdditions with multiple methods

---
+++ Example/Tests/KFXUIColorAdditionsTests.m
@@ -0,0 +1,30 @@
+
+
+#import <XCTest/XCTest.h>
+#import "UIColor+KFXAdditions.h"
+
+@interface KFXUIColorAdditionsTests : XCTestCase
+
+@end
+
+@implementation KFXUIColorAdditionsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+
+
+
+
+
+
+@end
